e5cc57bc8ef41714c741240ddc951fb7e4bf2c24
Diogo Garbinato <diogogarbinato@gmail.com>
2023-08-31 16:03:29 -0300

    ex005

---
+++ ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um número: '))
+
+print('Você digitou:', n)
+print('O antecessor de {} é {}'.format(n, n - 1))
+print('O sucessor de {} é {}'.format(n, (n + 1)))
