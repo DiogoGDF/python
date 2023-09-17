f87a5070800af2451169085b95824206e34b278a
Diogo Garbinato <diogogarbinato@gmail.com>
2023-09-17 10:08:50 -0300

    ex020

---
+++ ex020.py
@@ -0,0 +1,16 @@
+'''
+Um professor quer sortear a ordem de apresentação de trabalhos dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''
+import random
+
+aluno1 = input('Nome do aluno 1: ')
+aluno2 = input('Nome do aluno 2: ')
+aluno3 = input('Nome do aluno 3: ')
+aluno4 = input('Nome do aluno 4: ')
+
+alunos = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(alunos)
+
+for i in alunos:
+  print(i)
