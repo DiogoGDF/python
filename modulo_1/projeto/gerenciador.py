2d2a03b4005a695c0588ab95b73fd48450d16c9b
Diogo Garbinato <diogogarbinato@gmail.com>
2024-08-05 19:33:40 -0300

    feat: funcionalidades em funções e melhorias

--- modulo_1/projeto/gerenciador.py
+++ modulo_1/projeto/gerenciador.py
@@ -2,6 +2,54 @@ import os
 import time
 lista_tarefas = []
 
+def strike(text):
+    return '\u0336'.join(text) + '\u0336'
+
+def imprimir_tarefas(contagem):
+    print("\n--- Tarefas ---")
+    if contagem:
+        for cont in range(len(lista_tarefas)):
+            print(f'\n{cont + 1} - {lista_tarefas[cont][0]} {lista_tarefas[cont][1]}')
+        tarefa = int(input("\nDigite o número da tarefa: ")) - 1
+        print('\n')
+        return tarefa
+        
+    else: 
+        for tarefa in lista_tarefas:
+            print(f'\n {tarefa[0]} {tarefa[1]}')
+        print('\n')
+
+def adicionar_tarefa():
+    inpt = input("Digite a tarefa: ")
+    tarefa = ["[ ]", inpt]
+    lista_tarefas.append(tarefa)
+
+def atualizar_tarefa():
+    tarefa_update = imprimir_tarefas(True)
+    tarefa = input("Digite a tarefa atualizada: ")
+    lista_tarefas[tarefa_update][1] = tarefa
+
+def completar_tarefa():
+    tarefa_completa = imprimir_tarefas(True)
+    lista_tarefas[tarefa_completa] = [lista_tarefas[tarefa_completa][0].replace(" ", "✓", 1), strike(lista_tarefas[tarefa_completa][1])]
+    os.system("clear")
+    imprimir_tarefas(False)
+    time.sleep(0.6)
+
+def remover_tarefas():
+    imprimir_tarefas(False)
+    time.sleep(0.4)
+    remover = []
+    for index in range(len(lista_tarefas)):
+        if lista_tarefas[index][0][1] == "✓":
+            remover.insert(0, index)
+
+    for index in remover:
+        lista_tarefas.pop(index)
+
+    os.system("clear")
+    imprimir_tarefas(False)
+    time.sleep(0.5)
 
 while True:
     os.system("clear")
@@ -18,54 +66,20 @@ while True:
     os.system("clear")
 
     if escolha == "1":
-        tarefa = input("Digite a tarefa: ")
-        lista_tarefas.append("[ ] " + tarefa)
+        adicionar_tarefa()
 
     elif escolha == "2":
-        print("\n--- Tarefas ---")
-        print("\n".join(lista_tarefas))
-
+        imprimir_tarefas(False)
         input("\nPrecione enter para voltar ")
 
     elif escolha == "3":
-        print("\n--- Tarefas ---")
-
-        for count in range(len(lista_tarefas)):
-            print(str(count + 1) + " - " + lista_tarefas[count])
-        
-        tarefa_update = int(input("\nDigite o número da tarefa a ser atualizada: ")) - 1
-        tarefa = input("Digite a tarefa atualizada: ")
-        lista_tarefas[tarefa_update] = "[ ] " + tarefa
+        atualizar_tarefa()
 
     elif escolha == "4":
-        print("\n--- Tarefas ---")
-        for count in range(len(lista_tarefas)):
-            print(str(count + 1) + " - " + lista_tarefas[count])
-
-        tarefa_completa = int(input("\nDigite o número da tarefa a completada: ")) - 1
-        lista_tarefas[tarefa_completa] = lista_tarefas[tarefa_completa].replace(" ", "x", 1)
-        os.system("clear")
-        print("\n--- Tarefas ---")
-        print("\n".join(lista_tarefas))
-        time.sleep(0.6)
+        completar_tarefa()
 
     elif escolha == "5":
-        print("\n--- Tarefas ---")
-        print("\n".join(lista_tarefas))
-        time.sleep(0.4)
-        remover = []
-        for index in range(len(lista_tarefas)):
-            if lista_tarefas[index][1] == "x":
-                remover.insert(0, index)
-
-        for index in remover:
-            lista_tarefas.pop(index)
-
-        os.system("clear")
-        print("\n--- Tarefas ---")
-        print("\n".join(lista_tarefas))
-        time.sleep(0.5)
-
+        remover_tarefas()
 
     elif escolha == "6":
         break
